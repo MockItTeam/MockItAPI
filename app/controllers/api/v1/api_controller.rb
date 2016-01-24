73e7d3b685eb30be908f6ea295e31905bd616635
maixezer <maixezer@gmail.com>
2016-01-24 16:18:54 +0700

    Add ability for access image by user_id

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::ApiController < ActionController::Base
 
   skip_before_action :verify_authenticity_token
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { render json: { message: exception.message }, status: :unauthorized  }
+    end
+  end
+
   def current_user
     return unless doorkeeper_token
     @current_user ||= User.where(id: doorkeeper_token.resource_owner_id).first
@@ -10,5 +16,10 @@ class Api::V1::ApiController < ActionController::Base
 
   def authenticate_user!
     head :unauthorized unless current_user
+  end
+
+  def page_params
+    @page = params[:page] || 1
+    @per_page = params[:per_page] || 25
   end
 end
