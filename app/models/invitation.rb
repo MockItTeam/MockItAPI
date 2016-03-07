95810c064039784a0d5cad4eac31d31f7cb46738
maixezer <maixezer@gmail.com>
2016-03-07 19:19:48 +0700

    Fix relationships models.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,12 @@
 class Invitation < ActiveRecord::Base
+  acts_as_paranoid
   enum status: %w(pending accepted refused cancelled)
 
-  belongs_to :from, class_name: 'User', foreign_key: 'from_user_id'
-  belongs_to :to, class_name: 'User', foreign_key: 'to_user_id'
+  belongs_to :sender, class_name: 'User', foreign_key: 'from_user_id'
+  belongs_to :recipient, class_name: 'User', foreign_key: 'to_user_id'
   belongs_to :project
 
-  validates_presence_of :from, :to, :project
+  validates_presence_of :sender, :recipient, :project
 
   before_create :set_default_status
 
