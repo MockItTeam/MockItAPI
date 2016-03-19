57537d71f3d087be1389d5d3c44ebe15ca008d28
maixezer <maixezer@gmail.com>
2016-03-19 12:52:42 +0700

    Use carrierwave_background in uploader, and use delayed_job to handle_asynchronous for image_processing.

--- app/models/raw_image.rb
+++ app/models/raw_image.rb
@@ -1,8 +1,9 @@
 class RawImage < ActiveRecord::Base
   acts_as_paranoid
   mount_uploader :name, RawImageUploader
+  process_in_background :name
   validates_processing_of :name
 
-  has_one :mockup
+  has_one :mockup, dependent: :destroy
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
 end
