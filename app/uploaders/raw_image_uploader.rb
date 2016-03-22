a136101004667d88662ad68f3107663f2399b42c
maixezer <maixezer@gmail.com>
2016-03-22 16:57:28 +0700

    Change mockup image upload to local.

--- app/uploaders/raw_image_uploader.rb
+++ app/uploaders/raw_image_uploader.rb
@@ -3,9 +3,10 @@
 class RawImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   include ::CarrierWave::Backgrounder::Delay
+  storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    '/var/images'
   end
 
   # Create different versions of your uploaded files:
