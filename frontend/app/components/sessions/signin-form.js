13eac7f2138b40c575e37a5f01e96f71041107bd
maixezer <maixezer@gmail.com>
2016-04-05 01:30:42 +0700

    Set app layout, css stylesheet for navbar

--- frontend/app/components/sessions/signin-form.js
+++ frontend/app/components/sessions/signin-form.js
@@ -4,11 +4,14 @@ const { service } = Ember.inject;
 
 export default Ember.Component.extend({
   session: service('session'),
+  password: new Array(4),
 
   actions: {
     authenticate() {
       this._resetForm();
-      let { identification, password } = this.getProperties('identification', 'password');
+      let identification = this.get('identification');
+      let password = this.get('password').join('');
+
       this
         .get('session')
         .authenticate('authenticator:oauth2', identification, password)
@@ -19,6 +22,17 @@ export default Ember.Component.extend({
             this.set('errorMessage', response.error);
           }
         });
+    },
+
+    moveOnMax(data, event) {
+      let field = event.target;
+      let fieldId = parseInt(field.id);
+
+      if (field.value.length == 1 && fieldId < 4) {
+        this.get('password')[fieldId] = data;
+        fieldId += 1;
+        $('#' + fieldId).focus();
+      }
     }
   },
 
