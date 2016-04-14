73253232b5ea291b167a4065a663bb07eb78eda3
maixezer <maixezer@gmail.com>
2016-04-15 03:58:08 +0700

    Can Upload mockup via website.

--- frontend/app/components/projects/show-form.js
+++ frontend/app/components/projects/show-form.js
@@ -3,10 +3,39 @@ const { service } = Ember.inject;
 
 export default Ember.Component.extend({
   classNames: ['project-container'],
+  store: service('store'),
 
   actions: {
     searchMockup(data, event) {
       this.sendAction('searchMockup', data);
+    },
+
+    createMockup() {
+      let mockup = this.get('store')
+        .createRecord('mockup', {
+          project: this.get('project')
+        });
+
+      mockup.save()
+        .then(() => {
+          this.sendAction('createMockup', mockup);
+        });
+    },
+
+    createMockupWithPhoto(files) {
+      let mockup = this.get('store')
+        .createRecord('mockup', {
+          project: this.get('project')
+        });
+
+      if (files[0]) {
+        mockup.set('image', files[0]);
+      }
+
+      mockup.save()
+        .then(() => {
+          console.log('create success');
+        });
     }
   }
 });
