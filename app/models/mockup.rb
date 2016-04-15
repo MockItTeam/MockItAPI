9719e6063e02f6678f17fdb96748429418fb728c
maixezer <maixezer@gmail.com>
2016-04-16 04:58:11 +0700

    Fix test mockup upload with image.

--- app/models/mockup.rb
+++ app/models/mockup.rb
@@ -2,13 +2,13 @@ class Mockup < ActiveRecord::Base
   acts_as_paranoid
   enum status: %w(pending in_progress created error)
 
-  belongs_to :raw_image, dependent: :destroy
+  belongs_to :raw_image
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   belongs_to :project
 
   validates_presence_of :project, :owner
   validate :json_format, on: :update, unless: :raw_image?
-  validate :status_created?, on: :update
+  validate :status_created_or_error?, on: :update
 
   validates :name,
             length: {in: 0..20},
@@ -16,7 +16,8 @@ class Mockup < ActiveRecord::Base
   validates_uniqueness_of :name, scope: :project
 
   before_validation :set_default_name
-  after_create :image_processing
+  after_create :before_process_image
+  before_update :before_process_image, if: Proc.new { |mockup| mockup.status == 'error' }
 
   scope :recently, -> { order(updated_at: :desc) }
 
@@ -36,8 +37,8 @@ class Mockup < ActiveRecord::Base
     end
   end
 
-  def status_created?
-    errors.add(:base, 'Cannot update, because this mockup is not created or still in progress.') unless self.status == 'created'
+  def status_created_or_error?
+    errors.add(:base, 'Cannot update, because this mockup is not created or still in progress.') unless self.status == 'created' || self.status == 'error'
   end
 
   def raw_image?
@@ -48,52 +49,46 @@ class Mockup < ActiveRecord::Base
     errors.add(:json_elements, 'is not in json format') unless json_elements.is_json?
   end
 
-  def image_processing
-    if self.raw_image.present?
-      self.status = :in_progress
-      self.save(validate: false)
+  def before_process_image
+    if raw_image?
+      update_columns(status: Mockup.statuses[:in_progress])
+      process_image
     else
-      self.status = :created
-      self.save(validate: false)
-      return
+      update_columns(status: Mockup.statuses[:created])
     end
+  end
 
+  def process_image
     begin
       command = "python ~/ElementDetector/main.py -f \"#{self.raw_image.name.url}\""
       result = %x(python ~/ElementDetector/main.py -f "#{self.raw_image.name.url}")
+
+      message = {}
+
       if result == nil
-        message = {}
         message["exec_command"] = command
         message["reason"] = "output from processor is nil (#{result})"
-        self.error_message = message.to_json
-        self.status = :error
       else
         result_hash = JSON.parse(result)
         if result_hash["error_message"]
-          self.error_message = result_hash["error_message"].to_json
-          self.status = :error
+          update_columns(status: Mockup.statuses[:error], error_message: result_hash["error_message"].to_json)
         elsif result_hash["json_elements"]
-          self.json_elements = result_hash["json_elements"].to_json
-          self.status = :created
+          update_columns(status: Mockup.statuses[:created], json_elements: result_hash["json_elements"].to_json)
         else
-          message = {}
           message["exec_command"] = command
           message["output_from_processor"] = result
-          self.error_message = message.to_json
-          self.status = :error
         end
       end
+      update_columns(status: Mockup.statuses[:error], error_message: message.to_json)
+
     rescue Exception => e
       message = {}
       message["exec_command"] = command
       message["output_from_processor"] = result
       message["rails_exception"] = e.backtrace.to_s
-      self.error_message = message.to_json
-      self.status = :error
+      update_columns(status: Mockup.statuses[:error], error_message: message.to_json)
     end
-
-    self.save(validate: false)
   end
 
-  handle_asynchronously :image_processing, :run_at => Proc.new { 1.seconds.from_now }
+  handle_asynchronously :process_image, :run_at => Proc.new { 1.seconds.from_now }
 end
