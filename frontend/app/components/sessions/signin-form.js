42f3a85a873c22428eab0cbdabe0c630c7d14b86
mapfap <mapfaps@gmail.com>
2016-04-16 02:01:09 +0700

    Improve UX for login page (PC and Android)

--- frontend/app/components/sessions/signin-form.js
+++ frontend/app/components/sessions/signin-form.js
@@ -4,8 +4,23 @@ const { service } = Ember.inject;
 
 export default Ember.Component.extend({
   session: service('session'),
-  password: new Array(4),
+  password: [-1, -1, -1, -1],
+  hold: [false],
+  showGuide: function(destination) {
+    var div = document.getElementById(destination);
+    var interval = 30;
+    var distance = 3;
+    var times = 5;
 
+    $(div).css('position', 'relative');
+
+    for (var iter = 0; iter < (times + 1) ; iter++) {
+        $(div).animate({
+            left: ((iter % 2 == 0 ? distance : distance * -1))
+        }, interval);
+    }                                                                                                          
+    $(div).animate({ left: 0 }, interval);
+  },
   actions: {
     authenticate() {
       this._resetForm();
@@ -24,19 +39,59 @@ export default Ember.Component.extend({
         });
     },
 
+    typeUsername(data, event) {
+      if (event.keyCode == 13) {
+        event.preventDefault();
+        $('#0').focus();
+      }
+    },
+
+    typePinDown(data, event) {
+      if (event.keyCode == 13 || event.keyCode == 9) {
+        event.preventDefault();
+      }
+    },
+
+    typeLogin(data, event) {
+      if (event.keyCode == 8) {
+        event.preventDefault();
+        $('#3').focus();
+        this.get('hold')[0] = true;
+      }
+    },
+
     typePin(data, event) {
       let field = event.target;
       let fieldId = parseInt(field.id);
 
-      if (field.value.length == 1 && event.keyCode != 8) {
-        this.get('password')[fieldId] = data;
-        fieldId += 1;
+      if ((event.keyCode == 8 && this.get('hold')[0] == false) || event.keyCode == 37) {
+        this.get('password')[fieldId] = -1;
+        $('#' + fieldId).removeClass('input-done');
+        if (fieldId <= 0) {
+          $('#username-field').focus();
+        } else {
+          $('#' + (fieldId-1)).focus();
+        }
+      } else if (event.keyCode >= 48 && event.keyCode <= 57) {
+        this.get('password')[fieldId] = event.keyCode - 48;
+        $('#' + fieldId).val('');
+        $('#' + fieldId).addClass('input-done');
+        if (fieldId >= 3) {
+          $('#login-btn').focus();
+        } else {
+          $('#' + (fieldId+1)).focus();
+        }
       } else {
-        this.get('password')[fieldId] = '';
-        fieldId -= 1;
+        this.get('showGuide')(fieldId);
+        // $('#' + fieldId).effect('shake');
       }
 
-      $('#' + fieldId).focus();
+      this.get('hold')[0] = false;
+    },
+
+    onFocus(data, event) {
+      $(this).val('');
+      $(this).removeClass('input-done');
     }
   },
 
