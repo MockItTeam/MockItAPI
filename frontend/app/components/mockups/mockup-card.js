efe711a9b881a8574f1872ad3a567e51c28d6c61
maixezer <maixezer@gmail.com>
2016-04-16 07:43:50 +0700

    Fix pollster

--- frontend/app/components/mockups/mockup-card.js
+++ frontend/app/components/mockups/mockup-card.js
@@ -7,32 +7,104 @@ export default Ember.Component.extend({
   classNames: ['app-card'],
   store: service('store'),
 
-  _setupPollster: Ember.on('didInsertElement', function() {
-    let _self = this;
-
-    if (Ember.isNone(this.get('pollster'))) {
-      _self.set('pollster', Pollster.create({
-        onPoll() {
-          _self.get('mockup').reload();
-        }
-      }));
-    }
-
-    this.get('pollster').start();
-  }),
-
-  _statusObserver: Ember.observer(
-    'mockup.status',
+  _setupPollster: Ember.on(
+    'didInsertElement',
     function() {
-      let mockupStatus = this.get('mockup.status');
-      if (mockupStatus == 'created') {
-        this.$('.in-progress-tab').attr('hidden', 'hidden');
-        this.$().removeAttr('disabled');
-        this.get('pollster').stop();
-      } else {
-        this.$('.in-progress-tab').removeAttr('hidden');
-        this.$().attr('disabled', 'disabled');
+      let _self = this;
+
+      if (Ember.isNone(this.get('pollster'))) {
+        _self.set('pollster', Pollster.create({
+          onPoll() {
+            _self.get('mockup')
+              .reload()
+              .then(() => {
+
+              }, () => {
+                _self.get('pollster').stop();
+                _self.get('pollster').destroy();
+              });
+          }
+        }));
       }
+      this.get('pollster').start();
+    }),
+
+  _checkMockupFound: Ember.on('didRender',
+    Ember.observer('mockup',
+      function() {
+        if (!this.get('mockup') && this.get('pollster')) {
+          this.get('pollster').stop();
+          this.get('pollster').destroy();
+        }
+      })),
+
+  _statusObserver: Ember.on(
+    'didInsertElement',
+    'didRender',
+    Ember.observer('mockup.status',
+      function() {
+        let mockupStatus = this.get('mockup.status');
+        let spanText = '';
+
+        this._resetTab();
+
+        if (mockupStatus == 'created') {
+          if (this.get('pollster'))
+            this.get('pollster').stop();
+        }
+
+        if (mockupStatus == 'in_progress') {
+          this.$('.tab').removeAttr('hidden');
+          console.log('in progress');
+          spanText = 'In Progress...';
+        }
+
+        if (mockupStatus == 'error') {
+          this.$('.tab').removeAttr('hidden');
+          this.$('.btn-tab').addClass('retry');
+          this.$('.tab-menu').addClass('dropdown');
+          spanText = 'Retry...';
+        }
+
+        this.$('.btn-tab').html(spanText);
+      }
+    )),
+
+  willDestroyElement() {
+    if (!Ember.isNone(this.get('pollster'))) {
+      this.get('pollster').stop();
     }
-  )
+    this.get('pollster').destroy();
+  },
+
+  _resetTab() {
+    this.$('.tab').attr('hidden', 'hidden');
+    this.$('.btn-tab').removeClass('retry');
+    this.$('.tab-menu').removeClass('dropdown');
+  },
+
+  actions: {
+
+    retryProcessMockup(mockup) {
+      let _self = this;
+
+      _self.get('pollster').stop();
+      mockup.save()
+        .then(() => {
+          _self._resetTab();
+          _self.get('pollster').start();
+        });
+    },
+
+    deleteMockup(mockup) {
+      let _self = this;
+
+      _self.get('pollster').stop();
+      mockup.deleteRecord();
+      mockup.save()
+        .then(() => {
+          this.sendAction('deleteMockup', `Delete mockup '${mockup.get('name')}' successfully.`)
+        });
+    }
+  }
 });
